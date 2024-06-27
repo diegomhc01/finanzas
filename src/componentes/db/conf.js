b3c6f1e53d52e354a5d92a53b153ac8802abe4aa
Diego <diegomhc01@gmail.com>
2024-06-27 18:15:22 -0300

    Update conf.js

--- src/componentes/db/conf.js
+++ src/componentes/db/conf.js
@@ -4,10 +4,4 @@ const App = express();
 
 
 export default App;
-//app.listen(3000, () => console.log(`Server running on port 3000`));
-/*
-diegomhc01:4odsU3K9j7PjpB9Z
-
-conn = 'mongodb+srv://diegomhc01:4odsU3K9j7PjpB9Z@cluster0.ilvgtbm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-*/
 
